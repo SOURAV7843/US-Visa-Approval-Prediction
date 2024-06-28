c16cb8a909917fa5a69813a2490a04e92b9168e3
SOURAV7843 <souravsamanta.1352ss@gmail.com>
2024-06-28 23:32:59 +0530

    Create the project Structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sourav Samanta",
+    author_email="sourasamanta.aiml@gmail.com",
+    packages=find_packages()
+)
